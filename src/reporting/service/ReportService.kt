c167531edcebb9ef35f5fa21a8b45db01262e891
Jonas Carlsen <jonas.carlsen@knowit.no>
2020-07-31 15:27:56 +0200

    Documentation for reporting service

--- src/reporting/service/ReportService.kt
+++ src/reporting/service/ReportService.kt
@@ -1,16 +1,11 @@
 package ombruk.backend.reporting.service
 
-import arrow.core.leftIfNull
 import ombruk.backend.calendar.model.Event
 import ombruk.backend.reporting.form.ReportUpdateForm
 import ombruk.backend.reporting.database.ReportRepository
 import ombruk.backend.reporting.form.ReportGetForm
-import ombruk.backend.shared.error.ServiceError
-import org.jetbrains.exposed.sql.transactions.transaction
-import org.slf4j.LoggerFactory
 
 object ReportService : IReportService {
-    private val logger = LoggerFactory.getLogger("ombruk.reporting.service.PartnerService")
 
     override fun saveReport(event: Event) = ReportRepository.insertReport(event)
 
