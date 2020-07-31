c7d0d1fdb4d613af7dbfc9ba985570044f86225c
Per Buer <per.buer@gmail.com>
2020-07-31 09:31:02 +0200

    remove invalid import statement as well as unused imports

--- src/pickup/form/GetPickupsForm.kt
+++ src/pickup/form/GetPickupsForm.kt
@@ -1,14 +1,7 @@
 package ombruk.backend.pickup.form
 
-import arrow.core.Either
-import calendar.form.EventGetForm
 import io.ktor.locations.KtorExperimentalLocationsAPI
 import io.ktor.locations.Location
-import kotlinx.serialization.Serializable
-import ombruk.backend.shared.error.ValidationError
-import ombruk.backend.shared.form.IForm
-import ombruk.backend.shared.model.serializer.LocalDateTimeSerializer
-import ombruk.backend.shared.utils.validation.runCatchingValidation
 import java.time.LocalDateTime
 
 @KtorExperimentalLocationsAPI
