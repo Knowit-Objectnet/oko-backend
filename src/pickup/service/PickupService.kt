116e86b9200cd7b3b86af7bf471714bb3d0f0978
Jonas Carlsen <jonas.carlsen@knowit.no>
2020-08-04 10:18:20 +0200

    Adding a chosenPartner to a pickup now creates an event

--- src/pickup/service/PickupService.kt
+++ src/pickup/service/PickupService.kt
@@ -1,7 +1,13 @@
 package ombruk.backend.pickup.service
 
+import arrow.core.left
+import arrow.core.right
+import ombruk.backend.calendar.form.event.EventPostForm
+import ombruk.backend.calendar.service.EventService
 import ombruk.backend.pickup.database.PickupRepository
 import ombruk.backend.pickup.form.pickup.*
+import ombruk.backend.pickup.model.Pickup
+import org.jetbrains.exposed.sql.transactions.transaction
 
 
 object PickupService : IPickupService {
@@ -13,7 +19,18 @@ object PickupService : IPickupService {
 
     override fun getPickups(pickupQueryForm: PickupGetForm) = PickupRepository.getPickups(pickupQueryForm)
 
-    override fun updatePickup(pickupUpdateForm: PickupUpdateForm) = PickupRepository.updatePickup(pickupUpdateForm)
+    override fun updatePickup(pickupUpdateForm: PickupUpdateForm) = transaction{
+        pickupUpdateForm.chosenPartnerID?.let {
+            var pickup: Pickup? = null
+            PickupRepository.updatePickup(pickupUpdateForm)
+                .map {
+                    pickup = it
+                    val eventPostForm = EventPostForm(it.startDateTime, it.endDateTime, it.station.id, it.chosenPartner!!.id)
+                    EventService.saveEvent(eventPostForm)
+                }
+                .fold({rollback(); it.left()}, {pickup!!.right()})
+        } ?: PickupRepository.updatePickup(pickupUpdateForm)
+    }
 
     override fun deletePickup(pickupDeleteForm: PickupDeleteForm) = PickupRepository.deletePickup(pickupDeleteForm.id)
 
