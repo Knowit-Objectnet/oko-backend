3c5256a9004d6901e27e548373bf000148de7af5
Aleksander Azizi <aleksander.azizi@knowit.no>
2021-05-04 09:45:03 +0200

    Changed Kontakt domain params to be optional.

--- src/aktor/domain/model/KontaktFindParams.kt
+++ src/aktor/domain/model/KontaktFindParams.kt
@@ -5,6 +5,6 @@ import ombruk.backend.core.domain.model.FindParams
 abstract class KontaktFindParams : FindParams {
     abstract override val id: Int
     abstract val navn: String
-    abstract val telefon: String
-    abstract val rolle: String
+    abstract val telefon: String?
+    abstract val rolle: String?
 }
--- src/aktor/domain/model/KontaktUpdateParams.kt
+++ src/aktor/domain/model/KontaktUpdateParams.kt
@@ -5,6 +5,6 @@ import ombruk.backend.core.domain.model.UpdateParams
 abstract class KontaktUpdateParams : UpdateParams {
     abstract override val id: Int
     abstract val navn: String
-    abstract val telefon: String
-    abstract val rolle: String
+    abstract val telefon: String?
+    abstract val rolle: String?
 }
