e76ccfc7c1914d9030584aab4c106a03fedcd4c7
Eirik Osnes <eirik.osnes@knowit.no>
2021-05-28 10:54:51 +0200

    Added error logging to RepositoryBase

--- src/core/infrastructure/RepositoryBase.kt
+++ src/core/infrastructure/RepositoryBase.kt
@@ -15,9 +15,12 @@ import org.jetbrains.exposed.sql.ResultRow
 import org.jetbrains.exposed.sql.deleteWhere
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
+import org.slf4j.LoggerFactory
 import java.util.*
 
 abstract class RepositoryBase<Entity : Any, EntityParams, EntityUpdateParams: UpdateParams, EntityFindParams : FindParams> {
+
+    private val logger = LoggerFactory.getLogger("ombruk.backend.core.infrastructure.RepositoryBase")
 
     abstract fun insertQuery(params: EntityParams): EntityID<UUID>
 
@@ -34,6 +37,7 @@ abstract class RepositoryBase<Entity : Any, EntityParams, EntityUpdateParams: Up
         return runCatching {
             insertQuery(params)
         }
+            .onFailure { logger.error("Failed to insert into database; ${it.message}") }
             .fold(
                 { findOne(it.value) },
                 { RepositoryError.InsertError("SQL error").left() }
@@ -43,42 +47,51 @@ abstract class RepositoryBase<Entity : Any, EntityParams, EntityUpdateParams: Up
     fun update(params: EntityUpdateParams): Either<RepositoryError, Entity> {
         return runCatching {
             updateQuery(params)
-        }.fold(
-            //Return right if more than 1 partner has been updated. Else, return an Error
-            {
-                Either.cond(it > 0,
-                    { findOne(params.id) },
-                    { RepositoryError.NoRowsFound("${params.id} not found") })
-            },
-            { RepositoryError.UpdateError(it.message).left() })
+        }
+            .onFailure { logger.error("Failed to update database; ${it.message}") }
+            .fold(
+                //Return right if more than 1 partner has been updated. Else, return an Error
+                {
+                    Either.cond(it > 0,
+                        { findOne(params.id) },
+                        { RepositoryError.NoRowsFound("${params.id} not found") })
+                },
+                { RepositoryError.UpdateError(it.message).left() }
+            )
             .flatMap { it }
     }
 
     fun findOne(id: UUID): Either<RepositoryError, Entity> {
         return runCatching {
             table.select { table.id eq id }.mapNotNull { toEntity(it) }
-        }.fold(
-            {
-                if (it.isNotEmpty()) it.first().right() else RepositoryError.NoRowsFound("$id not found").left()
-            },
-            { RepositoryError.NoRowsFound("SQL error").left() }
-        )
+        }
+            .onFailure { logger.error("Failed to findOne in database; ${it.message}") }
+            .fold(
+                {
+                    if (it.isNotEmpty()) it.first().right() else RepositoryError.NoRowsFound("$id not found").left()
+                },
+                { RepositoryError.NoRowsFound("SQL error").left() }
+            )
     }
 
     fun delete(id: UUID): Either<RepositoryError, Unit> {
         return runCatching {
             table.deleteWhere { table.id eq id }
-        }.fold(
-            { Unit.right() },
-            { RepositoryError.DeleteError(it.message).left() }
-        )
+        }
+            .onFailure { logger.error("Failed to delete from database; ${it.message}") }
+            .fold(
+                { Unit.right() },
+                { RepositoryError.DeleteError(it.message).left() }
+            )
 
     }
 
     fun find(params: EntityFindParams): Either<RepositoryError, List<Entity>> = runCatching {
         prepareQuery(params).mapNotNull { toEntity(it) }
-    }.fold(
-        { it.right() },
-        { RepositoryError.SelectError(it.message).left() }
-    )
+    }
+        .onFailure { logger.error("Failed to find in database; ${it.message}") }
+        .fold(
+            { it.right() },
+            { RepositoryError.SelectError(it.message).left() }
+        )
 }
