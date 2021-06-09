7fcdd422919ccc8b6724d7f23423c1b88923b12d
Eirik Osnes <eirik.osnes@knowit.no>
2021-06-09 12:21:24 +0200

    Verifying only 1 partner accepted at a time.

--- src/utlysning/application/api/dto/UtlysningPartnerAcceptDto.kt
+++ src/utlysning/application/api/dto/UtlysningPartnerAcceptDto.kt
@@ -1,10 +1,16 @@
 package ombruk.backend.utlysning.application.api.dto
 
+import arrow.core.Either
 import io.ktor.locations.*
 import kotlinx.serialization.Serializable
 import ombruk.backend.shared.form.IForm
 import ombruk.backend.shared.utils.validation.runCatchingValidation
 import ombruk.backend.utlysning.domain.params.UtlysningPartnerAcceptParams
+import ombruk.backend.utlysning.domain.port.IUtlysningRepository
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+import org.valiktor.functions.isValid
 import org.valiktor.validate
 import shared.model.serializer.UUIDSerializer
 import java.util.*
@@ -15,9 +21,19 @@ import java.util.*
 data class UtlysningPartnerAcceptDto(
     @Serializable(with = UUIDSerializer::class) override val id: UUID,
     override val toAccept: Boolean
-) : IForm<UtlysningPartnerAcceptDto>, UtlysningPartnerAcceptParams() {
+) : IForm<UtlysningPartnerAcceptDto>, UtlysningPartnerAcceptParams(), KoinComponent {
     override fun validOrError() = runCatchingValidation {
+        //TODO: This validation is based on a first-come-first-serve, if we want manual processing,
+        // we need a different validation
         validate(this) {
+
+            if (toAccept) {
+                val utlysningRepository: IUtlysningRepository by inject()
+                val allAccepted = transaction { utlysningRepository.find(UtlysningFindDto(partnerPameldt = true))}
+                require(allAccepted is Either.Right)
+
+                validate(UtlysningPartnerAcceptDto::toAccept).isValid { allAccepted.b.isEmpty() }
+            }
         }
     }
 }
--- test/integrationtest/UtlysningTest.kt
+++ test/integrationtest/UtlysningTest.kt
@@ -4,54 +4,60 @@ import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
 import io.mockk.mockkClass
+import ombruk.backend.aktor.aktorModule
 import ombruk.backend.aktor.application.api.dto.PartnerSaveDto
 import ombruk.backend.aktor.application.api.dto.StasjonSaveDto
-import ombruk.backend.aktor.application.service.PartnerService
-import ombruk.backend.aktor.application.service.StasjonService
+import ombruk.backend.aktor.application.service.IPartnerService
+import ombruk.backend.aktor.application.service.IStasjonService
 import ombruk.backend.aktor.domain.entity.Partner
 import ombruk.backend.aktor.domain.entity.Stasjon
 import ombruk.backend.aktor.domain.enum.StasjonType
-import ombruk.backend.aktor.infrastructure.repository.PartnerRepository
-import ombruk.backend.aktor.infrastructure.repository.StasjonRepository
 import ombruk.backend.henting.application.api.dto.EkstraHentingFindDto
 import ombruk.backend.henting.application.api.dto.EkstraHentingSaveDto
-import ombruk.backend.henting.application.service.EkstraHentingService
+import ombruk.backend.henting.application.service.IEkstraHentingService
 import ombruk.backend.henting.domain.entity.EkstraHenting
-import ombruk.backend.henting.infrastructure.repository.EkstraHentingRepository
+import ombruk.backend.henting.hentingModule
 import ombruk.backend.shared.api.KeycloakGroupIntegration
 import ombruk.backend.utlysning.application.api.dto.UtlysningBatchSaveDto
-import ombruk.backend.utlysning.application.service.UtlysningService
+import ombruk.backend.utlysning.application.api.dto.UtlysningPartnerAcceptDto
+import ombruk.backend.utlysning.application.service.IUtlysningService
 import ombruk.backend.utlysning.domain.entity.Utlysning
-import ombruk.backend.utlysning.infrastructure.repository.UtlysningRepository
+import ombruk.backend.utlysning.utlysningModule
 import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.extension.ExtendWith
+import org.koin.core.context.loadKoinModules
+import org.koin.core.context.startKoin
+import org.koin.test.KoinTest
+import org.koin.test.get
 import org.testcontainers.junit.jupiter.Testcontainers
 import testutils.TestContainer
 import java.time.LocalDateTime
 import java.util.*
 import kotlin.test.assertEquals
+import kotlin.test.assertNull
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation::class)
 @ExtendWith(MockKExtension::class)
 @Testcontainers
-class UtlysningTest {
+class UtlysningTest : KoinTest {
     private val testContainer = TestContainer()
-    private val partnerRepository = PartnerRepository()
-    private val stasjonRepository = StasjonRepository()
-    private val ekstraHentingRepository = EkstraHentingRepository()
-    private val ekstraHentingService = EkstraHentingService(ekstraHentingRepository)
-    private val utlysningRepository = UtlysningRepository()
-    private val utlysningService = UtlysningService(utlysningRepository)
-    private lateinit var partnerService: PartnerService
-    private lateinit var stasjonService: StasjonService
+    private lateinit var ekstraHentingService: IEkstraHentingService
+    private lateinit var utlysningService: IUtlysningService
+    private lateinit var partnerService: IPartnerService
+    private lateinit var stasjonService: IStasjonService
     private var keycloakGroupIntegration = mockkClass(KeycloakGroupIntegration::class)
 
     @BeforeAll
     fun setup() {
         testContainer.start()
-        stasjonService = StasjonService(stasjonRepository, keycloakGroupIntegration)
-        partnerService = PartnerService(keycloakGroupIntegration, partnerRepository)
+        startKoin {  }
+        loadKoinModules(listOf(hentingModule, aktorModule, utlysningModule))
+        stasjonService = get()
+        partnerService = get()
+        ekstraHentingService = get()
+        utlysningService = get()
     }
 
     private lateinit var stasjon1: Stasjon
@@ -130,12 +136,37 @@ class UtlysningTest {
 
     }
 
+    @Test
+    @Order(5)
+    fun testAcceptUtlysning() {
 
-    /*TODO:
-    * Test accepting of Utlysning
-    * Get all currently accepted Utlysning
-    * Stasjon confirm Utlysning
-    * ...
-    * */
+        //Tests the partner accept functionality (stasjonAccept currently not in use because of first-pass-the-post).
+        //End result should be that utlysninger1[1] is accepted.
+
+        val partnerAcceptDto1 = UtlysningPartnerAcceptDto(utlysninger1[0].id, true).validOrError()
+        require(partnerAcceptDto1 is Either.Right)
+
+        val accepted1 = utlysningService.partnerAccept(partnerAcceptDto1.b)
+        println(accepted1)
+        require(accepted1 is Either.Right)
+        assertNotNull(accepted1.b.partnerPameldt)
+
+        val partnerAcceptDto2 = UtlysningPartnerAcceptDto(utlysninger1[1].id, true).validOrError()
+        require(partnerAcceptDto2 is Either.Left)
+
+        val partnerAcceptDto3 = UtlysningPartnerAcceptDto(utlysninger1[0].id, false).validOrError()
+        require(partnerAcceptDto3 is Either.Right)
+
+        val unAccept = utlysningService.partnerAccept(partnerAcceptDto3.b)
+        require(unAccept is Either.Right)
+        assertNull(unAccept.b.partnerPameldt)
+
+        val partnerAcceptDto4 = UtlysningPartnerAcceptDto(utlysninger1[1].id, true).validOrError()
+        require(partnerAcceptDto4 is Either.Right)
+
+        val accepted2 = utlysningService.partnerAccept(partnerAcceptDto4.b)
+        require(accepted2 is Either.Right)
+        assertNotNull(accepted2.b.partnerPameldt)
+    }
 
 }
