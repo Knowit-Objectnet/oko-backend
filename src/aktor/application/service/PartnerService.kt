0d293943e3ef8f2f82adae00564547f768228d3b
Eirik Osnes <eirik.osnes@knowit.no>
2021-06-11 18:43:40 +0200

    StasjonService.kt and PartnerService.kt now updates all dependent entities

--- src/aktor/application/service/PartnerService.kt
+++ src/aktor/application/service/PartnerService.kt
@@ -1,26 +1,34 @@
 package ombruk.backend.aktor.application.service
 
-import arrow.core.*
+import arrow.core.Either
 import arrow.core.extensions.either.applicative.applicative
 import arrow.core.extensions.either.monad.flatMap
 import arrow.core.extensions.list.traverse.sequence
-import io.ktor.locations.KtorExperimentalLocationsAPI
-import io.ktor.util.KtorExperimentalAPI
+import arrow.core.left
+import arrow.core.right
+import io.ktor.locations.*
+import io.ktor.util.*
 import ombruk.backend.aktor.application.api.dto.KontaktGetDto
 import ombruk.backend.aktor.application.api.dto.PartnerGetDto
 import ombruk.backend.aktor.application.api.dto.PartnerSaveDto
 import ombruk.backend.aktor.application.api.dto.PartnerUpdateDto
 import ombruk.backend.aktor.domain.entity.Partner
 import ombruk.backend.aktor.domain.port.IPartnerRepository
+import ombruk.backend.avtale.application.api.dto.AvtaleFindDto
+import ombruk.backend.avtale.application.service.IAvtaleService
 import ombruk.backend.shared.api.KeycloakGroupIntegration
 import ombruk.backend.shared.error.ServiceError
+import ombruk.backend.utlysning.application.api.dto.UtlysningFindDto
+import ombruk.backend.utlysning.application.service.IUtlysningService
 import org.jetbrains.exposed.sql.transactions.transaction
 import java.util.*
 
 class PartnerService constructor(
     private val keycloakGroupIntegration: KeycloakGroupIntegration,
     private val partnerRepository: IPartnerRepository,
-    private val kontaktService: IKontaktService
+    private val kontaktService: IKontaktService,
+    private val avtaleService: IAvtaleService,
+    private val utlysningService: IUtlysningService
 ) : IPartnerService {
 
     @KtorExperimentalAPI
@@ -77,7 +85,7 @@ class PartnerService constructor(
     //TODO: Handle Keycloak logic: Should probably be the same as delete.
     override fun archiveOne(id: UUID): Either<ServiceError, Unit> {
         return transaction { partnerRepository.archiveOne(id)
-            .map{
+            .map{ partner ->
                     kontaktService.getKontakter(KontaktGetDto(aktorId = id))
                         .map {
                             it.map { kontakt ->
@@ -86,8 +94,11 @@ class PartnerService constructor(
                                 .sequence(Either.applicative())
                                 .flatMap { Either.Right(Unit) }
                         }
+                        .flatMap { it }
+                        .map { avtaleService.archive(AvtaleFindDto(aktorId = partner.id)) }.flatMap { it }
+                        .map { utlysningService.archive(UtlysningFindDto(partnerId = partner.id)) }.flatMap { it }
                 }
-            .flatMap { it.flatMap { it } }
+            .flatMap { it }
             .fold({rollback(); it.left()}, { it.right()})
         }
     }
