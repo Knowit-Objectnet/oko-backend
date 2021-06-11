93873a75b341ed396568873731c8c72dbb46baed
aspleym <aspleym@gmail.com>
2021-06-11 10:29:53 +0200

    Viderebygging på testene

--- test/unittest/aktor/kontakt/repository/KontaktRepositoryTest.kt
+++ test/unittest/aktor/kontakt/repository/KontaktRepositoryTest.kt
@@ -6,6 +6,7 @@ import arrow.core.rightIfNotNull
 import ombruk.backend.aktor.domain.entity.Kontakt
 import ombruk.backend.aktor.domain.model.KontaktCreateParams
 import ombruk.backend.aktor.domain.model.KontaktFindParams
+import ombruk.backend.aktor.domain.model.KontaktUpdateParams
 import ombruk.backend.aktor.infrastructure.repository.KontaktRepository
 import ombruk.backend.shared.error.RepositoryError
 import org.jetbrains.exposed.sql.transactions.transaction
@@ -17,6 +18,8 @@ import org.testcontainers.junit.jupiter.Testcontainers
 import testutils.TestContainer
 import java.util.*
 import kotlin.test.assertEquals
+import kotlin.test.assertFails
+import kotlin.test.assertNotEquals
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @Testcontainers
@@ -80,5 +83,78 @@ class KontaktRepositoryTest {
         println(find)
         require(find is Either.Right)
         assert(find.b.isEmpty())
+    }
+
+    @Test
+    fun testFindOneInserted() {
+        val aktorId = UUID.randomUUID()
+        val kontakt = object: KontaktCreateParams() {
+            override val aktorId: UUID = aktorId
+            override val navn: String = "Oslo"
+            override val telefon: String = "40404040"
+            override val epost: String = "leder@oslo.no"
+            override val rolle: String = "Leder"
+        }
+
+        var kontaktObj: Kontakt? = null
+
+        transaction {
+            val insert = kontaktRepository.insert(kontakt)
+            val k = insert.fold({ null }, {it})
+            kontaktObj = k
+        }
+
+        kontaktObj?.let {
+            val findOne = transaction { kontaktRepository.findOne(kontaktObj!!.id) }
+            require(findOne is Either.Right)
+            assertEquals(kontaktObj, findOne.b )
+        }
+        kontaktObj ?: assert(false)
+    }
+
+    @Test
+    fun testUpdate() {
+        val aktorId = UUID.randomUUID()
+        val kontakt = object: KontaktCreateParams() {
+            override val aktorId: UUID = aktorId
+            override val navn: String = "Oslo"
+            override val telefon: String = "40404040"
+            override val epost: String = "leder@oslo.no"
+            override val rolle: String = "Leder"
+        }
+
+        var kontaktObj: Kontakt? = null
+
+        transaction {
+            val insert = kontaktRepository.insert(kontakt)
+            val k = insert.fold({ null }, {it})
+            kontaktObj = k
+        }
+
+        kontaktObj?.let {
+            val updateParam = object: KontaktUpdateParams() {
+                override val id: UUID = kontaktObj!!.id
+                override val aktorId: UUID = kontaktObj!!.aktorId
+                override val navn: String = kontaktObj!!.navn
+                override val telefon: String? = kontaktObj!!.telefon
+                override val epost: String? = kontaktObj!!.epost
+                override val rolle: String = "Sjåfør"
+            }
+            transaction {
+                val update = kontaktRepository.update(updateParam)
+                val u = update.fold({ null }, { it })
+
+
+                u.let {
+                    assertNotEquals(u, kontaktObj)
+                    assertEquals(u!!.id, kontaktObj!!.id)
+                    assertNotEquals(u.rolle, kontaktObj!!.rolle)
+                }
+                // If update is null
+                u ?: assert(false)
+            }
+        }
+        // If inserted kontakt is null
+        kontaktObj ?: assert(false)
     }
 }
