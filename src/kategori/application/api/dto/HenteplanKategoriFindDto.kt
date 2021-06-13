27be7ecb4c658cdf14ef5c4fbca8da348b47a2ef
Aleksander Azizi <aleksander.azizi@knowit.no>
2021-06-13 18:49:54 +0200

    Kategorier integrert med Henteplan.

---
+++ src/kategori/application/api/dto/HenteplanKategoriFindDto.kt
@@ -0,0 +1,28 @@
+package ombruk.backend.kategori.application.api.dto
+
+import arrow.core.Either
+import io.ktor.locations.*
+import kotlinx.serialization.Serializable
+import ombruk.backend.kategori.domain.params.HenteplanKategoriFindParams
+import ombruk.backend.shared.error.ValidationError
+import ombruk.backend.shared.form.IForm
+import ombruk.backend.shared.utils.validation.runCatchingValidation
+import org.valiktor.validate
+import shared.model.serializer.UUIDSerializer
+import java.util.*
+
+@KtorExperimentalLocationsAPI
+@Location("/{henteplanId}/kategorier")
+@Serializable
+data class HenteplanKategoriFindDto(
+    @Serializable(with = UUIDSerializer::class) override val id: UUID? = null,
+    @Serializable(with = UUIDSerializer::class) override var henteplanId: UUID? = null,
+    @Serializable(with = UUIDSerializer::class) override val kategoriId: UUID? = null,
+    override val merknad: String? = null
+) : IForm<HenteplanKategoriFindDto>,
+    HenteplanKategoriFindParams() {
+    override fun validOrError(): Either<ValidationError, HenteplanKategoriFindDto> = runCatchingValidation {
+        validate(this) {
+        }
+    }
+}
--- src/kategori/application/api/dto/KategoriFindDto.kt
+++ src/kategori/application/api/dto/KategoriFindDto.kt
@@ -1,18 +1,11 @@
 package ombruk.backend.kategori.application.api.dto
 
-import arrow.core.Either
 import io.ktor.locations.*
-import kotlinx.serialization.Serializable
-import ombruk.backend.aktor.application.api.dto.StasjonFindDto
-import ombruk.backend.aktor.domain.enum.StasjonType
-import ombruk.backend.aktor.domain.model.StasjonFindParams
 import ombruk.backend.kategori.domain.params.KategoriFindParams
-import ombruk.backend.shared.error.ValidationError
 import ombruk.backend.shared.form.IForm
 import ombruk.backend.shared.utils.validation.runCatchingValidation
 import org.valiktor.functions.isNotBlank
 import org.valiktor.validate
-import shared.model.serializer.UUIDSerializer
 import java.util.*
 
 @KtorExperimentalLocationsAPI
