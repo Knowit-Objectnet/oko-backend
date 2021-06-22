290d08d6415941b86aa25bd6e419b242932d8e9e
Eirik Osnes <eirik.osnes@knowit.no>
2021-06-22 12:08:36 +0200

    Updated integration test to better test ekstrautlysning use case

--- test/integrationtest/UtlysningTest.kt
+++ test/integrationtest/UtlysningTest.kt
@@ -1,5 +1,4 @@
 import arrow.core.Either
-import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
 import ombruk.backend.aktor.application.api.dto.PartnerSaveDto
 import ombruk.backend.aktor.application.api.dto.StasjonSaveDto
@@ -16,6 +15,7 @@ import ombruk.backend.henting.domain.entity.EkstraHenting
 import ombruk.backend.henting.hentingModule
 import ombruk.backend.kategori.kategoriModule
 import ombruk.backend.utlysning.application.api.dto.UtlysningBatchSaveDto
+import ombruk.backend.utlysning.application.api.dto.UtlysningFindDto
 import ombruk.backend.utlysning.application.api.dto.UtlysningPartnerAcceptDto
 import ombruk.backend.utlysning.application.service.IUtlysningService
 import ombruk.backend.utlysning.domain.entity.Utlysning
@@ -32,8 +32,11 @@ import org.testcontainers.junit.jupiter.Testcontainers
 import testutils.MockAktorModule
 import testutils.TestContainer
 import java.time.LocalDateTime
+import java.util.*
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
 import kotlin.test.assertNull
+import kotlin.test.assertTrue
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation::class)
@@ -67,25 +70,29 @@ class UtlysningTest : KoinTest {
     private lateinit var stasjon2: Stasjon
     private lateinit var partner1: Partner
     private lateinit var partner2: Partner
+    private lateinit var partner3: Partner
     private lateinit var ekstraHenting: EkstraHenting
     private lateinit var utlysninger1: List<Utlysning>
 
     @Test
     @Order(1)
-    fun setupPartnerAndStasjon(@MockK expected: Any) {
+    fun setupPartnerAndStasjon() {
 
         val partnerInsert1 = partnerService.savePartner(PartnerSaveDto(navn = "TestPartner1", ideell = true))
         val partnerInsert2 = partnerService.savePartner(PartnerSaveDto(navn = "TestPartner2", ideell = true))
+        val partnerInsert3 = partnerService.savePartner(PartnerSaveDto(navn = "TestPartner3", ideell = true))
         val stasjonInsert1 = stasjonService.save(StasjonSaveDto("TestStasjon1", StasjonType.GJENBRUK))
         val stasjonInsert2 = stasjonService.save(StasjonSaveDto("TestStasjon2", StasjonType.MINI))
 
         require(partnerInsert1 is Either.Right)
         require(partnerInsert2 is Either.Right)
+        require(partnerInsert3 is Either.Right)
         require(stasjonInsert1 is Either.Right)
         require(stasjonInsert2 is Either.Right)
 
         partner1 = partnerInsert1.b
         partner2 = partnerInsert2.b
+        partner3 = partnerInsert3.b
         stasjon1 = stasjonInsert1.b
         stasjon2 = stasjonInsert2.b
     }
@@ -98,9 +105,10 @@ class UtlysningTest : KoinTest {
             sluttTidspunkt = LocalDateTime.of(2021,6,3,12,30),
             merknad = null,
             stasjonId = stasjon1.id
-        )
+        ).validOrError()
+        require(ekstraHentingSaveDto is Either.Right)
 
-        val ehSave = ekstraHentingService.save(ekstraHentingSaveDto)
+        val ehSave = ekstraHentingService.save(ekstraHentingSaveDto.b)
         require(ehSave is Either.Right)
 
         ekstraHenting = ehSave.b
@@ -126,9 +134,22 @@ class UtlysningTest : KoinTest {
         val utlysningBatchPostDto = UtlysningBatchSaveDto(
             hentingId = ekstraHenting.id,
             partnerIds = listOf(partner1.id.toString(), partner2.id.toString())
-        )
+        ).validOrError()
+        require(utlysningBatchPostDto is Either.Right)
 
-        val batchPost = utlysningService.batchSave(utlysningBatchPostDto)
+        val utlysningBatchPostDtoWrong1 = UtlysningBatchSaveDto(
+            hentingId = ekstraHenting.id,
+            partnerIds = listOf(partner1.id.toString(), "This is not a legal UUID")
+        ).validOrError()
+        assert(utlysningBatchPostDtoWrong1 is Either.Left)
+
+        val utlysningBatchPostDtoWrong2 = UtlysningBatchSaveDto(
+            hentingId = ekstraHenting.id,
+            partnerIds = listOf(partner1.id.toString(), UUID.randomUUID().toString())
+        ).validOrError()
+        assert(utlysningBatchPostDtoWrong2 is Either.Left)
+
+        val batchPost = utlysningService.batchSave(utlysningBatchPostDto.b)
         require(batchPost is Either.Right)
         utlysninger1 = batchPost.b
 
@@ -138,7 +159,63 @@ class UtlysningTest : KoinTest {
     }
 
     @Test
-    @Order(5)
+    @Order(4)
+    fun testAddMoreUtlysninger() {
+        assertEquals(2, utlysninger1.size)
+
+        val utlysningBatchSaveDto1 = UtlysningBatchSaveDto(
+            hentingId = ekstraHenting.id,
+            partnerIds = listOf(partner1.id.toString(), partner2.id.toString())
+        ).validOrError()
+        require(utlysningBatchSaveDto1 is Either.Right)
+
+        val batchPostSame = utlysningService.batchSave(utlysningBatchSaveDto1.b)
+        require(batchPostSame is Either.Right)
+        assertEquals(0, batchPostSame.b.size)
+
+        val utlysningBatchSaveDto2 = UtlysningBatchSaveDto(
+            hentingId = ekstraHenting.id,
+            partnerIds = listOf(partner1.id.toString(), partner2.id.toString(), partner3.id.toString())
+        ).validOrError()
+        require(utlysningBatchSaveDto2 is Either.Right)
+
+        val batchPostSameAndNew = utlysningService.batchSave(
+            utlysningBatchSaveDto2.b
+        )
+        require(batchPostSameAndNew is Either.Right)
+        assertEquals(1, batchPostSameAndNew.b.size)
+
+
+        val utlysningFindDto = UtlysningFindDto(hentingId = ekstraHenting.id).validOrError()
+        require(utlysningFindDto is Either.Right)
+
+        val findUtlysninger = utlysningService.find(utlysningFindDto.b)
+        require(findUtlysninger is Either.Right)
+        utlysninger1 = findUtlysninger.b
+
+        assert(utlysninger1.any { it.partnerId == partner1.id })
+        assert(utlysninger1.any { it.partnerId == partner2.id })
+        assert(utlysninger1.any { it.partnerId == partner3.id })
+    }
+
+    @Test
+    @Order(6)
+    fun testFindNoneAccepted() {
+        //Assert that none have been accepted yet, and that this shows in both utlysninger and EkstraHenting:
+
+        //Utlysning
+        assertFalse(utlysninger1.any { it.partnerPameldt != null })
+
+        //EkstraHenting
+        val findOne = ekstraHentingService.findOne(ekstraHenting.id)
+        require(findOne is Either.Right)
+        assertNull( findOne.b.godkjentUtlysning )
+    }
+
+    lateinit var acceptedUtlysning: Utlysning
+
+    @Test
+    @Order(7)
     fun testAcceptUtlysning() {
 
         //Tests the partner accept functionality (stasjonAccept currently not in use because of first-pass-the-post).
@@ -168,6 +245,29 @@ class UtlysningTest : KoinTest {
         val accepted2 = utlysningService.partnerAccept(partnerAcceptDto4.b)
         require(accepted2 is Either.Right)
         assertNotNull(accepted2.b.partnerPameldt)
+        acceptedUtlysning = accepted2.b
+    }
+
+    @Test
+    @Order(8)
+    fun testFindAccepted() {
+        //Assert that none have been accepted yet, and that this shows in both utlysninger and EkstraHenting:
+
+        //Utlysning
+        val utlysningFindDto = UtlysningFindDto(hentingId = ekstraHenting.id).validOrError()
+        require(utlysningFindDto is Either.Right)
+
+        val findUtlysninger = utlysningService.find(utlysningFindDto.b)
+        require(findUtlysninger is Either.Right)
+        utlysninger1 = findUtlysninger.b
+
+        assertTrue(utlysninger1.any { it.partnerPameldt != null })
+
+        //EkstraHenting
+        val findOne = ekstraHentingService.findOne(ekstraHenting.id)
+        require(findOne is Either.Right)
+        assertNotNull( findOne.b.godkjentUtlysning )
+        assertEquals(acceptedUtlysning ,findOne.b.godkjentUtlysning)
     }
 
 }
