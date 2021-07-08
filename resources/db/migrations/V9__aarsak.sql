770fe157e67284a8ad96ebff1b7dead173f02d69
Adrian Pleym <adrian.pleym@knowit.no>
2021-07-08 14:18:35 +0200

    📦 NEW: Oppdatert databasen

---
+++ resources/db/migrations/V9__aarsak.sql
@@ -0,0 +1,14 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE aarsak (
+    id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+    beskrivelse text NOT NULL
+);
+
+ALTER TABLE planlagt_henting
+DROP COLUMN aarsak;
+
+ALTER TABLE planlagt_henting
+ADD COLUMN aarsak_id uuid,
+FOREIGN KEY (aarsak_id) REFERENCES aarsak;
+
