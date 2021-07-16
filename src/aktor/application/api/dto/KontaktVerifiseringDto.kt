72f2affdba37c3ff939d0aa6401d81766ed3d74a
Eirik Osnes <eirik.osnes@knowit.no>
2021-07-16 14:43:11 +0200

    Added validation of verification codes

--- src/aktor/application/api/dto/KontaktVerifiseringDto.kt
+++ src/aktor/application/api/dto/KontaktVerifiseringDto.kt
@@ -2,13 +2,10 @@ package ombruk.backend.aktor.application.api.dto
 
 import io.ktor.locations.*
 import kotlinx.serialization.Serializable
-import ombruk.backend.aktor.domain.model.KontaktFindParams
-import ombruk.backend.aktor.domain.model.KontaktUpdateParams
 import ombruk.backend.aktor.domain.model.KontaktVerifiserParams
-import ombruk.backend.aktor.domain.model.VerifiseringUpdateParams
 import ombruk.backend.shared.form.IForm
+import ombruk.backend.shared.utils.validation.isValidVerificationCode
 import ombruk.backend.shared.utils.validation.runCatchingValidation
-import org.valiktor.functions.isNotBlank
 import org.valiktor.validate
 import shared.model.serializer.UUIDSerializer
 import java.util.*
@@ -21,6 +18,9 @@ data class KontaktVerifiseringDto(
     override val epostKode: String? = null,
 ) : IForm<KontaktVerifiseringDto>, KontaktVerifiserParams() {
     override fun validOrError() = runCatchingValidation {
-        validate(this) {}
+        validate(this) {
+            validate(KontaktVerifiseringDto::telefonKode).isValidVerificationCode()
+            validate(KontaktVerifiseringDto::epostKode).isValidVerificationCode()
+        }
     }
 }
