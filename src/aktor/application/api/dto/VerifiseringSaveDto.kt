1c07793158f67b0f18cfcb3a542fed36dde5f4a7
Eirik Osnes <eirik.osnes@knowit.no>
2021-07-16 09:24:30 +0200

    Optimised some imports

--- src/aktor/application/api/dto/VerifiseringSaveDto.kt
+++ src/aktor/application/api/dto/VerifiseringSaveDto.kt
@@ -1,18 +1,10 @@
 package ombruk.backend.aktor.application.api.dto
 
-import arrow.core.Either
 import kotlinx.serialization.Serializable
-import ombruk.backend.aktor.application.service.IAktorService
-import ombruk.backend.aktor.domain.model.KontaktCreateParams
 import ombruk.backend.aktor.domain.model.VerifiseringCreateParams
 import ombruk.backend.shared.form.IForm
-import ombruk.backend.shared.utils.validation.isNorwegianPhoneNumberOrBlank
-import ombruk.backend.shared.utils.validation.isEmailOrBlank
 import ombruk.backend.shared.utils.validation.runCatchingValidation
 import org.koin.core.component.KoinComponent
-import org.koin.core.component.inject
-import org.valiktor.functions.isNotBlank
-import org.valiktor.functions.isValid
 import org.valiktor.validate
 import shared.model.serializer.UUIDSerializer
 import java.util.*
