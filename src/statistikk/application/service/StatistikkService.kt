b8dad6933a4e55593551dd916ba01a6b90ca9a99
Adrian Pleym <adrian.pleym@knowit.no>
2021-07-27 09:19:05 +0200

    Endret måten statistikk hentes ut

--- src/statistikk/application/service/StatistikkService.kt
+++ src/statistikk/application/service/StatistikkService.kt
@@ -3,21 +3,58 @@ package ombruk.backend.statistikk.application.service
 import arrow.core.Either
 import arrow.core.left
 import arrow.core.right
+import ombruk.backend.aktor.infrastructure.table.PartnerTable
+import ombruk.backend.aktor.infrastructure.table.StasjonTable
+import ombruk.backend.avtale.infrastructure.table.AvtaleTable
 import ombruk.backend.henting.application.api.dto.HentingFindDto
 import ombruk.backend.henting.application.service.IHentingService
+import ombruk.backend.henting.infrastructure.table.EkstraHentingTable
+import ombruk.backend.henting.infrastructure.table.HenteplanTable
+import ombruk.backend.henting.infrastructure.table.PlanlagtHentingTable
 import ombruk.backend.kategori.application.api.dto.KategoriFindDto
 import ombruk.backend.kategori.application.service.IKategoriService
 import ombruk.backend.kategori.domain.entity.Kategori
+import ombruk.backend.kategori.infrastructure.table.KategoriTable
 import ombruk.backend.shared.error.ServiceError
 import ombruk.backend.statistikk.application.api.dto.StatistikkFindDto
 import ombruk.backend.statistikk.domain.entity.KategoriStatistikk
 import ombruk.backend.statistikk.domain.entity.StasjonStatistikk
 import ombruk.backend.statistikk.domain.entity.Statistikk
+import ombruk.backend.vektregistrering.infrastructure.table.VektregistreringTable
+import org.h2.table.Plan
+import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
+import java.time.LocalDateTime
 import java.util.*
 
 class StatistikkService(val hentingService: IHentingService, val kategoriService: IKategoriService) : IStatistikkService {
+
+    fun queryStatistikkAggregert(dto: StatistikkFindDto): Query {
+        val stasjonAlias = StasjonTable.alias("stasjonAktorAlias")
+        val planlagtHenting = PlanlagtHentingTable
+            .innerJoin(HenteplanTable, { henteplanId }, { id })
+            .innerJoin(AvtaleTable, { HenteplanTable.avtaleId }, { id })
+            .innerJoin(StasjonTable, { HenteplanTable.stasjonId }, { id })
+            .leftJoin(PartnerTable, { AvtaleTable.aktorId }, { id })
+            .leftJoin(stasjonAlias, { AvtaleTable.aktorId }, { stasjonAlias[StasjonTable.id] })
+            .innerJoin(VektregistreringTable, { PlanlagtHentingTable.id }, { hentingId })
+            .innerJoin(KategoriTable, { VektregistreringTable.kategoriId }, { id })
+
+        val query = planlagtHenting.slice(PartnerTable.navn, StasjonTable.navn, KategoriTable.navn, KategoriTable.id, VektregistreringTable.vekt.sum()).selectAll()
+
+        dto.partnerId?.let{ query.andWhere { PartnerTable.id eq it } }
+        dto.stasjonId?.let { query.andWhere { StasjonTable.id eq it } }
+        dto.kategoriId?.let { query.andWhere { VektregistreringTable.kategoriId eq it } }
+        dto.after?.let { query.andWhere { PlanlagtHentingTable.startTidspunkt.greaterEq(it) } }
+        dto.before?.let { query.andWhere { PlanlagtHentingTable.sluttTidspunkt.lessEq(it) } }
+
+        query.groupBy(PartnerTable.navn, StasjonTable.navn, KategoriTable.navn, KategoriTable.id)
+
+        return query
+    }
+
     override fun find(dto: StatistikkFindDto): Either<ServiceError, List<Statistikk>> = transaction {
+        val query = queryStatistikkAggregert(dto)
 
         fun <T> List<T>.replace(newValue: T, block: (T) -> Boolean): List<T> {
             return map {
@@ -25,48 +62,22 @@ class StatistikkService(val hentingService: IHentingService, val kategoriService
             }
         }
 
-        val hentinger = hentingService.find(HentingFindDto(before = dto.before, after = dto.after, aktorId = dto.partnerId, stasjonId = dto.stasjonId))
-        val kategorier = kategoriService.find(KategoriFindDto());
-        val statistikkMap = mutableMapOf<UUID, Statistikk>()
+        val statistikkMap = mutableMapOf<String, Statistikk>()
 
-        hentinger.fold({it.left()}, {
-            it.filter { it.aktorId != null }.map {
+        query.mapNotNull {
+            val row = it
+            var statistikk = statistikkMap.get(row[PartnerTable.navn]) ?: Statistikk(partnerNavn = row[PartnerTable.navn])
+            var stasjon = statistikk.stasjoner.find { it.stasjonNavn == row[StasjonTable.navn] } ?: StasjonStatistikk(stasjonNavn = row[StasjonTable.navn])
+            stasjon = stasjon.copy(kategorier = stasjon.kategorier + KategoriStatistikk(kategoriId = row[KategoriTable.id].value, kategoriNavn = row[KategoriTable.navn], vekt = row[VektregistreringTable.vekt.sum()] ?: 0f))
 
-                val aktorId = it.aktorId!!
-                val aktorNavn = it.aktorNavn!!
-                val stasjonNavn = it.stasjonNavn
-                var vektregistreringer = it.ekstraHenting?.vektregistreringer ?: it.planlagtHenting?.vektregistreringer ?: emptyList()
+            if (statistikk.stasjoner.find { it.stasjonNavn == stasjon.stasjonNavn } != null) {
+                val newStasjoner =
+                    statistikk.stasjoner.replace(stasjon) { it.stasjonNavn == stasjon.stasjonNavn }
+                statistikk = statistikk.copy(stasjoner = newStasjoner)
+            } else statistikk = statistikk.copy(stasjoner = statistikk.stasjoner + stasjon)
 
-                var statistikk = statistikkMap.get(aktorId) ?: Statistikk(partnerNavn = aktorNavn)
-                var stasjon = statistikk.stasjoner.find { stasjon -> stasjon.stasjonNavn == stasjonNavn} ?: StasjonStatistikk(stasjonNavn)
-
-                if (dto.kategoriId != null) vektregistreringer = vektregistreringer.filter { it.kategoriId == dto.kategoriId }
-
-                vektregistreringer.map { registrering ->
-                    val hentingKategori: Kategori
-                    val kategoriFinder = kategorier.map { it.find { it.id == registrering.kategoriId } }
-                    require(kategoriFinder is Either.Right)
-                    hentingKategori = kategoriFinder.b!!
-
-                    var kategoriStatistikk: KategoriStatistikk = stasjon.kategorier.find { kategori ->  kategori.kategoriId == registrering.kategoriId } ?: KategoriStatistikk(kategoriId = hentingKategori.id, kategoriNavn = hentingKategori.navn)
-                    kategoriStatistikk = kategoriStatistikk.copy(vekt = kategoriStatistikk.vekt + registrering.vekt)
-
-                    if (stasjon.kategorier.find { kategori ->  kategori.kategoriId == registrering.kategoriId } != null) {
-                        val newKategorier = stasjon.kategorier.replace(kategoriStatistikk) {it.kategoriId == kategoriStatistikk.kategoriId}
-                        stasjon = stasjon.copy(kategorier = newKategorier)
-                    } else stasjon = stasjon.copy(kategorier = stasjon.kategorier + kategoriStatistikk)
-                }
-
-                if (statistikk.stasjoner.find { it.stasjonNavn == stasjon.stasjonNavn } != null) {
-                    val newStasjoner =
-                        statistikk.stasjoner.replace(stasjon) { it.stasjonNavn == stasjon.stasjonNavn }
-                    statistikk = statistikk.copy(stasjoner = newStasjoner)
-                } else statistikk = statistikk.copy(stasjoner = statistikk.stasjoner + stasjon)
-
-                statistikkMap.put(aktorId, statistikk)
-
-            }
-        })
+            statistikkMap.put(statistikk.partnerNavn, statistikk)
+        }
         statistikkMap.values.toList().right()
     }
 }
