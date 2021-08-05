32ae3fab879c36cf92a8413252cd878b9943fd9d
Adrian Pleym <adrian.pleym@knowit.no>
2021-08-05 12:34:32 +0200

    Kategori uuiden som konstant

--- src/kategori/application/service/KategoriService.kt
+++ src/kategori/application/service/KategoriService.kt
@@ -6,6 +6,7 @@ import arrow.core.left
 import arrow.core.right
 import ombruk.backend.kategori.application.api.dto.*
 import ombruk.backend.kategori.domain.entity.Kategori
+import ombruk.backend.kategori.domain.model.ANDRE_OMBRUKSVARER_UUID_STRING
 import ombruk.backend.kategori.domain.port.IKategoriRepository
 import ombruk.backend.shared.error.ServiceError
 import org.jetbrains.exposed.sql.transactions.transaction
@@ -60,7 +61,7 @@ class KategoriService(
 
     override fun update(dto: KategoriUpdateDto): Either<ServiceError, Kategori> {
         return transaction {
-            if (dto.id == UUID.fromString("0f3f3bdd-5733-45da-87ae-a9417596cb12")) ServiceError("Illegal category to update").left()
+            if (dto.id == UUID.fromString(ANDRE_OMBRUKSVARER_UUID_STRING)) ServiceError("Illegal category to update").left()
             else kategoriRepository.update(dto)
         }
     }
---
+++ src/kategori/domain/model/KategoriConstants.kt
@@ -0,0 +1,3 @@
+package ombruk.backend.kategori.domain.model
+
+const val ANDRE_OMBRUKSVARER_UUID_STRING: String = "0f3f3bdd-5733-45da-87ae-a9417596cb12"
