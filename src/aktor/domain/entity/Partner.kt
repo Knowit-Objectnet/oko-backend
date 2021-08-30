b8463e6f4fcbc1369981dd3f92bdd5051d9c2e30
Lars Kåre Syversen <lars-kare.syversen@knowit.no>
2021-08-30 10:53:55 +0200

    endepunkt for partner kan nå inkludere informasjon om avtaler

--- src/aktor/domain/entity/Partner.kt
+++ src/aktor/domain/entity/Partner.kt
@@ -1,6 +1,7 @@
 package ombruk.backend.aktor.domain.entity
 
 import kotlinx.serialization.Serializable
+import ombruk.backend.avtale.domain.entity.Avtale
 import shared.model.serializer.UUIDSerializer
 import java.util.*
 
@@ -9,5 +10,6 @@ data class Partner(
     @Serializable(with = UUIDSerializer::class) override var id: UUID,
     override var navn: String,
     override var kontaktPersoner: List<Kontakt>,
+    var avtaler: List<Avtale> = emptyList(),
     var ideell: Boolean
 ): IAktor
